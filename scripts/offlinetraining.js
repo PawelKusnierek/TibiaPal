3bb9766d66965cafc5593a242204fce1d30f038c
Pawel Kusnierek <31161519+PawelKusnierek@users.noreply.github.com>
2020-12-27 19:28:46 +0100

    initial commit for offline training tool - not yet working or publicly available

---
+++ scripts/offlinetraining.js
@@ -0,0 +1,13 @@
+function submit_form() {
+    offlinetrainingformresults = document.getElementById("offlinetrainingformresults")
+    offlinetrainingformresults.innerHTML = ""
+
+
+    vocation = document.getElementById("vocation").value
+    skilltype = document.getElementById("skilltype").value
+    currentskill = document.getElementById("currentskill").value
+    currentskillpercentage = document.getElementById("currentskillpercentage").value
+    targetskill = document.getElementById("targetskill").value
+    loyalty = document.getElementById("loyalty").value
+
+}
