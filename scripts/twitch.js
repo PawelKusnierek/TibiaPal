a725d0b8bf554e701168fa39166571b8afcd9d00
Pawel Kusnierek <31161519+PawelKusnierek@users.noreply.github.com>
2021-03-18 11:27:08 +0100

    Delete twitch.js

--- scripts/twitch.js
+++
@@ -1,9 +0,0 @@
-function find_stream_status() {
-    stream_content = document.getElementById("stream-content")
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api.twitch.tv/helix/streams/kusnierr", true);
-    xhr.setRequestHeader("Client-ID", "qqiafa1gpappsi6jz2yq056ajcuxnu");
-    var data = JSON.parse(xhr.responseText);
-    var a = 1;
-}
