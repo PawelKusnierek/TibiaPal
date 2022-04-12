55d7055b77bba5120ee75006f30cbdd003972910
Pawel Kusnierek <31161519+PawelKusnierek@users.noreply.github.com>
2022-04-12 18:27:10 +0200

    Disallowing cases where level <1

--- scripts/expcalculator.js
+++ scripts/expcalculator.js
@@ -1,29 +1,41 @@
 function submit_form() {
-    expcalculatorresults = document.getElementById("expcalculatorresults")
-    expcalculatorresults.innerHTML = ""
+  expcalculatorresults = document.getElementById("expcalculatorresults")
+  expcalculatorresults.innerHTML = ""
 
-    startinglevel = document.getElementById("startinglevel").value
-    targetlevel = document.getElementById("targetlevel").value
+  startinglevel = document.getElementById("startinglevel").value
+  targetlevel = document.getElementById("targetlevel").value
 
-    total_experience_needed = calculate_experience(startinglevel, targetlevel)
-
+  total_experience_needed = calculate_experience(startinglevel, targetlevel)
+  if (typeof total_experience_needed === 'string' ) {
+    expcalculatorresults.innerHTML = total_experience_needed
+  }
+  else {
     formatted_experience = numberWithSpaces(total_experience_needed)
     expcalculatorresults.innerHTML = "Total experience needed to get from level <b>" + startinglevel + "</b> to <b>" + targetlevel + "</b> is:<br><br> <b><span class=\"orange\">" + formatted_experience + "</span></b><br>"
+  }
 }
-
+//Can return results off by one due to rounding, but not a significant difference
 function experience_for_level(level) {
-  // you start at lvl 1, its impossible to reach lvl <1, so we could do something like: if(level < 1){throw new Error("lvl <1 is impossible");}
-  if(level === 1){
-    // specialcase.
+  // special case n=1
+  if (level === 1) {
     return 0;
+  }
+  else if (level < 1) {
+    return "Levels less than one not possible"
   }
   return ((50 * Math.pow(level, 3)) / 3) - (100 * Math.pow(level, 2)) + (((850 * level) / 3) - 200);
 }
 function calculate_experience(startinglevel, targetlevel) {
-    // sometimes possibly off-by-1 due to rounding? i'm not sure
-    return Math.round(experience_for_level(targetlevel) - experience_for_level(startinglevel));
+  target_exp = experience_for_level(targetlevel)
+  starting_exp = experience_for_level(startinglevel)
+  if (typeof target_exp === 'string' || typeof starting_exp === 'string') {
+    return "Levels less than one not possible"
+  }
+  else {
+    return Math.round(target_exp) - Math.round(starting_exp);
+  }
 }
 
 function numberWithSpaces(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
