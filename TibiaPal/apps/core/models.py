5e5d13d9517f7fe5d0c96d0d6b89635db8739da8
Pawel Kusnierek <31161519+PawelKusnierek@users.noreply.github.com>
2022-04-23 13:25:46 +0200

    adding the World model as well as relevant functions for scraping and updating the db

--- TibiaPal/apps/core/models.py
+++ TibiaPal/apps/core/models.py
@@ -1,4 +1,6 @@
+from bs4 import BeautifulSoup
 from django.db import models
+import requests
 
 VOCATION_CHOICES = (
     ('KNIGHT', 'Knight'),
@@ -20,3 +22,31 @@ class Test_Character(models.Model):
     level = models.PositiveSmallIntegerField()
     achievement_points = models.PositiveSmallIntegerField()
     world = models.CharField(max_length=30)
+
+
+class World(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def scrape_worlds():
+        url = "https://www.tibia.com/community/?subtopic=worlds"
+
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        table = soup.find_all('table',  {'class': 'TableContent'})
+
+        rows = table[2].find_all('tr')
+        rows = rows[1:]
+        worlds = []
+        for row in rows:
+            world = World(name=row.next.text)
+            worlds.append(world)
+
+        return worlds
+
+    def update_worlds_db(worlds):
+        # Veryfing that the output makes sense, i.e. there are more than 50 worlds but less than 100. This should be true for a few years at least
+        if len(worlds) < 100 and len(worlds) > 50:
+            World.objects.delete()
+            World.objects.bulk_create(worlds)
