f4280375b944a92c6e8aa2b825dafe62e9affa36
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2023-03-23 13:26:22 +0100

    Adding extra options to bless calculator

--- TibiaPal/static/scripts/bless.js
+++ TibiaPal/static/scripts/bless.js
@@ -3,32 +3,60 @@ function submit_form() {
     content = document.getElementById("deathblessresults")
     content.innerHTML = "";
 
-    if (level < 121) {
-        regular_cost = regular_level_under_120(level)
-        enhanced = enhanced_level_under_120(level)
+    IsHenricus = document.getElementById("henricus").checked;
+    IsEnhanced = document.getElementById("enhanced").checked;
+    IsTwist = document.getElementById("twist").checked;
+
+    regular_cost = Math.round(regular_blesses(level, IsHenricus) / 1000)
+
+    if (IsEnhanced) {
+        enhanced_cost = Math.round(enhanced_blesses(level) / 1000)
     }
     else {
-        regular_cost = regular_level_over_120(level)
-        enhanced = enhanced_level_over_120(level)
+        enhanced_cost = 0
     }
 
-    total_cost = Math.round((regular_cost + enhanced) / 1000)
+    if (IsTwist) {
+        if (level > 270) {
+            twist_cost = 50000 / 1000
+        }
+        else {
+            twist_cost = Math.round((2000 + 200 * (level - 30)) / 1000)
+        }
+    }
+    else {
+        twist_cost = 0
+    }
 
-    content.innerHTML = content.innerHTML + "For a character with level " + level + ", blesses bought from Henricus (inq) plus two Enhanced blessings will cost:<br><br> <b>" + total_cost + "k</b>"
+    total_cost = Math.round(regular_cost + enhanced_cost + twist_cost)
+
+    content.innerHTML = content.innerHTML + "For a character with level " + level + ", total bless cost: <b>" + total_cost + "k</b> <br><br> Regular Bless: " + regular_cost + "k<br>Enhanced Bless: " + enhanced_cost + "k<br> Twist of Fate: " + twist_cost + "k"
 }
 
-function regular_level_under_120(level) {
-    return (1000 * (level - 20)) * 1.1
+function regular_blesses(level, IsHenricus) {
+    if (level < 121) {
+        if (IsHenricus) {
+            return (1000 * (level - 20)) * 1.1
+        }
+        else {
+            return 1000 * (level - 20)
+        }
+    }
+    else {
+        if (IsHenricus) {
+            return (100000 + 375 * (level - 120)) * 1.1
+        }
+        else {
+            return 100000 + 375 * (level - 120)
+        }
+    }
 }
 
-function enhanced_level_under_120(level) {
-    return 520 * (level - 20)
-}
-
-function regular_level_over_120(level) {
-    return (100000 + 375 * (level - 120)) * 1.1
-}
-
-function enhanced_level_over_120(level) {
-    return 52000 + 200 * (level - 120)
+function enhanced_blesses(level) {
+    if (level < 121) {
+        return 520 * (level - 20)
+    }
+    else {
+        return 52000 + 200 * (level - 120)
+    }
 }
