56a06122d67275ab8ef1485082f941e5f81e5743
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2024-07-07 22:41:09 +0200

    forge progress commit

--- scripts/forge.js
+++ scripts/forge.js
@@ -1,10 +1,36 @@
 var item_classification
-var current_tier
 var target_tier
 var target_price
 var feeder_price
 var exalted_price
 var sliver_price
+
+var fusion_cost = {
+  1: {
+    1: 25000
+  },
+  2: {
+    1: 750000,
+    2: 5000000
+  },
+  3: {
+    1: 4000000,
+    2: 10000000,
+    3: 20000000
+  },
+  4: {
+    1: 8000000,
+    2: 20000000,
+    3: 40000000,
+    4: 65000000,
+    5: 100000000,
+    6: 250000000,
+    7: 750000000,
+    8: 2500000000,
+    9: 8000000000,
+    10: 15000000000,
+  },
+}
 
 function submit_forge_form() {
   read_form_values()
@@ -12,11 +38,19 @@ function submit_forge_form() {
   if (verified == false) {
     return
   }
+  calculate_fusion()
+  calculate_transfer()
+  if (item_classification == 4) {
+    calculate_convergence_fusion()
+    calculate_convergence_transfer()
+  }
+  else {
+    pass
+  }
 }
 
 function read_form_values() {
   item_classification = document.getElementById("classification").value
-  current_tier = document.getElementById("current_tier").value
   target_tier = document.getElementById("target_tier").value
   target_price = document.getElementById("main_item_price").value
   feeder_price = document.getElementById("feeder_item_price").value
@@ -25,20 +59,16 @@ function read_form_values() {
 }
 
 function verify_entered_values() {
-  if (current_tier >= target_tier) {
-    window.alert("Current tier must be lower than Target tier.");
+  if (item_classification == 3 && target_tier > 3) {
+    window.alert("Tier cannot be greater than 3 for Item Classification 3.");
     return false
   }
-  if (item_classification == 3 && (current_tier > 3 || target_tier > 3)) {
-    window.alert("Current tier/Target tier cannot be greater than 3 for Item Classification 3.");
+  if (item_classification == 2 && target_tier > 2) {
+    window.alert("Tier cannot be greater than 2 for Item Classification 2.");
     return false
   }
-  if (item_classification == 2 && (current_tier > 2 || target_tier > 2)) {
-    window.alert("Current tier/Target tier cannot be greater than 2 for Item Classification 2.");
-    return false
-  }
-  if (item_classification == 1 && (current_tier > 1 || target_tier > 1)) {
-    window.alert("Current tier/Target tier cannot be greater than 1 for Item Classification 1.");
+  if (item_classification == 1 && target_tier > 1) {
+    window.alert("Tier cannot be greater than 1 for Item Classification 1.");
     return false
   }
   if (target_price == '') {
@@ -58,4 +88,15 @@ function verify_entered_values() {
     return false
   }
   return true
+}
+
+function calculate_fusion() {
+  var gold_spent = 0
+  var exalted_cores_used = 0
+  var sliver_used = 0
+  var dust_used = 0
+
+  for (var i = 0; i <= target_tier; i++) {
+    pass
+  }
 }
