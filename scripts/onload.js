63925fe5daf4a738c5b318a572d8fc24eee8d9c1
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2024-11-25 10:45:58 +0100

    improvements to charm planner (costs + tabs)

--- scripts/onload.js
+++ scripts/onload.js
@@ -2,6 +2,7 @@ function initialize() {
   //cookie_notice();
   //find_rashid_city();
   enable_expandable_div_buttons();
+  enable_default_tabs();
 }
 
 function find_rashid_city() {
@@ -106,4 +107,45 @@ function showAdblockPopup() {
 function adBlockPopupClicked() {
   document.querySelector(".adblockpopup").style.display = "none";
   localStorage.setItem("adblockconfirm", Date.now());
+}
+
+function enable_default_tabs(){
+  if (location.href.split("/").slice(-1).includes('charm_planner')) {
+    initial_show_tab("Major Charms")
+  }
+
+}
+function show_tab(evt, tab_name) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tab_name).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function initial_show_tab(tab_name) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tab_name).style.display = "block";
 }
