a1fab1abdf5b776b04b7847b822d7b7c1237257b
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2025-01-26 21:40:42 +0100

    commit filtering

--- _site/scripts/equipment.js
+++ _site/scripts/equipment.js
@@ -1,58 +1,43 @@
-function apply_level_filter() {
-	let elements = document.getElementsByName('level_filter_choice');
-	for (let i = 0; i < elements.length; i++) {
-		if (elements[i].checked) {
-			filter_equipment_table_by_level(parseInt(elements[i].value));
-			return;
+function apply_filters() {
+	let level_elements = document.getElementsByName('level_filter_choice');
+    let level_selection
+    let type_elements = document.getElementsByName('type_filter_choice');
+	for (let i = 0; i < level_elements.length; i++) {
+		if (level_elements[i].checked) {
+			level_selection = parseInt(level_elements[i].value);
 		}
 	}
+    for (let i = 0; i < type_elements.length; i++) {
+		if (type_elements[i].checked) {
+			type_selection = type_elements[i].value;
+		}
+	}
+    filter_equipment_table(level_selection, type_selection)
 }
 
-function filter_equipment_table_by_level(level_filter) {
+function filter_equipment_table(level_selection, type_selection) {
+    console.log(level_selection)
+    console.log(type_selection)
+
     let vocation = get_active_vocation()
 	let equipment_table = document.getElementById("equipment_table_" + vocation);
 	let number_of_rows = equipment_table.rows.length;
-	unfilter_equipment_table_by_level(vocation);
+	unfilter_equipment_table(vocation);
 	for (let i = number_of_rows - 1; i > 0; i--) {
 		let level_value = equipment_table.rows[i].cells[0].childNodes[0].data;
-		if (parseInt(level_value) < level_filter) {
+		if (parseInt(level_value) < level_selection) {
 			equipment_table.rows[i].style.display = 'none'
 		}
+        let type_value = equipment_table.rows[i].cells[2].childNodes[0].data;
+        if (type_selection != "All") {
+            if (type_value != type_selection) {
+                equipment_table.rows[i].style.display = 'none'
+            }
+        }
 	}
 }
 
-function unfilter_equipment_table_by_level(vocation) {
-	let equipment_table = document.getElementById("equipment_table_" + vocation);
-	let number_of_rows = equipment_table.rows.length;
-	for (let i = number_of_rows - 1; i > 0; i--) {
-		equipment_table.rows[i].style.display = 'table-row'
-	}
-}
-
-function apply_type_filter() {
-	let elements = document.getElementsByName('type_filter_choice');
-	for (let i = 0; i < elements.length; i++) {
-		if (elements[i].checked) {
-			filter_equipment_table_by_type(elements[i].value);
-			return;
-		}
-	}
-}
-
-function filter_equipment_table_by_type(type) {
-    let vocation = get_active_vocation()
-	let equipment_table = document.getElementById("equipment_table_" + vocation);
-	let number_of_rows = equipment_table.rows.length;
-	unfilter_equipment_table_by_level(vocation);
-	for (let i = number_of_rows - 1; i > 0; i--) {
-		let level_value = equipment_table.rows[i].cells[0].childNodes[0].data;
-		if (parseInt(level_value) < level_filter) {
-			equipment_table.rows[i].style.display = 'none'
-		}
-	}
-}
-
-function unfilter_equipment_table_by_type(vocation) {
+function unfilter_equipment_table(vocation) {
 	let equipment_table = document.getElementById("equipment_table_" + vocation);
 	let number_of_rows = equipment_table.rows.length;
 	for (let i = number_of_rows - 1; i > 0; i--) {
