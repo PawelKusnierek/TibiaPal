45fb8ebb1ffde8ca592f164e9c81f34da5386e47
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2025-01-29 09:02:19 +0100

    update filtering

--- _site/scripts/equipment.js
+++ _site/scripts/equipment.js
@@ -1,44 +1,68 @@
 function apply_filters() {
 	let vocation = get_active_vocation()
+
 	let level_elements = document.getElementsByName('level_filter_choice_' + vocation);
-    let level_selection
-    let type_elements = document.getElementsByName('type_filter_choice_' + vocation);
-	let type_selection
-    let weapon_elements = document.getElementsByName('weapon_filter_choice_knight');
-	let weapon_type_selection
+	let level_selection
 	for (let i = 0; i < level_elements.length; i++) {
 		if (level_elements[i].checked) {
-			level_selection = parseInt(level_elements[i].value);
+			level_selection = level_elements[i].value;
 		}
 	}
-    for (let i = 0; i < type_elements.length; i++) {
+
+	let type_elements = document.getElementsByName('type_filter_choice_' + vocation);
+	let type_selection
+	for (let i = 0; i < type_elements.length; i++) {
 		if (type_elements[i].checked) {
 			type_selection = type_elements[i].value;
 		}
 	}
+
+	let weapon_elements = document.getElementsByName('weapon_filter_choice_knight');
+	let weapon_type_selection
 	for (let i = 0; i < weapon_elements.length; i++) {
 		if (weapon_elements[i].checked) {
 			weapon_type_selection = weapon_elements[i].value;
 		}
 	}
-    filter_equipment_table(level_selection, type_selection, weapon_type_selection, vocation)
+	filter_equipment_table(level_selection, type_selection, weapon_type_selection, vocation)
 }
 
 function filter_equipment_table(level_selection, type_selection, weapon_type_selection, vocation) {
+	let apply_level_filter = false
+	if (level_selection != "0") {
+		apply_level_filter = true
+	}
+	let level_over = true
+	if (level_selection.includes("<")) {
+		level_over = false
+	}
+	level_selection = parseInt(level_selection.replace("<", "").replace("+", ""))
+
 	let equipment_table = document.getElementById("equipment_table_" + vocation);
 	let number_of_rows = equipment_table.rows.length;
 	unfilter_equipment_table(vocation);
 	for (let i = number_of_rows - 1; i > 0; i--) {
 		let level_value = equipment_table.rows[i].cells[0].childNodes[0].data;
-		if (parseInt(level_value) < level_selection) {
-			equipment_table.rows[i].style.display = 'none'
+		if (apply_level_filter) {
+			if (level_over) {
+				if (parseInt(level_value) < level_selection) {
+					equipment_table.rows[i].style.display = 'none'
+				}
+			}
+			else {
+				if (parseInt(level_value) > level_selection) {
+					equipment_table.rows[i].style.display = 'none'
+				}
+			}
 		}
-        let type_value = equipment_table.rows[i].cells[3].childNodes[0].data;
-        if (type_selection != "All") {
+
+		let type_value = equipment_table.rows[i].cells[3].childNodes[0].data;
+		if (type_selection != "All") {
 			if (!type_value.includes(type_selection)) {
-                equipment_table.rows[i].style.display = 'none'
-            }
-        }
+				equipment_table.rows[i].style.display = 'none'
+			}
+		}
+
 		if (vocation == "knight") {
 			if (weapon_type_selection != "All") {
 				let weapon_type_value = equipment_table.rows[i].cells[3].childNodes[0].data;
@@ -61,10 +85,10 @@ function unfilter_equipment_table(vocation) {
 }
 
 function get_active_vocation() {
-    let vocation_buttons = document.getElementsByClassName("tablinks");
-    for (i = 0; i < vocation_buttons.length; i++) {
+	let vocation_buttons = document.getElementsByClassName("tablinks");
+	for (i = 0; i < vocation_buttons.length; i++) {
 		if (vocation_buttons[i].classList.contains("active")) {
-            return vocation_buttons[i].id
-        }
+			return vocation_buttons[i].id
+		}
 	}
 }
