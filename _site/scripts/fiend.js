19bf3f897cd93d5781d0916f21a2c9bf125c502e
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2026-01-26 20:06:49 +0100

    improve fiend map buttons

--- _site/scripts/fiend.js
+++ _site/scripts/fiend.js
@@ -401,9 +401,9 @@ init()
 	// Up arrow button
 	const upButton = document.createElement("button");
 	upButton.id = "button-floor-up";
-	upButton.textContent = "↑";
-	upButton.disabled = true;
 	upButton.classList.add("tibia-map-button");
+	upButton.classList.add("tibia-map-arrow-up");
+	upButton.disabled = true;
 	upButton.addEventListener('click', () => {
 		this.goUpOneFloor();
 	});
@@ -413,18 +413,18 @@ init()
 	const floorDisplayButton = document.createElement("button");
 	floorDisplayButton.id = "button-floor-display";
 	floorDisplayButton.textContent = "0";
-	floorDisplayButton.disabled = true;
 	floorDisplayButton.classList.add("tibia-map-button");
 	floorDisplayButton.classList.add("tibia-map-button-highlight");
+	floorDisplayButton.classList.add("tibia-map-floor-display");
 	this.#floorDisplayButton = floorDisplayButton;
 	buttons.appendChild(floorDisplayButton);
 	
 	// Down arrow button
 	const downButton = document.createElement("button");
 	downButton.id = "button-floor-down";
-	downButton.textContent = "↓";
-	downButton.disabled = true;
 	downButton.classList.add("tibia-map-button");
+	downButton.classList.add("tibia-map-arrow-down");
+	downButton.disabled = true;
 	downButton.addEventListener('click', () => {
 		this.goDownOneFloor();
 	});
