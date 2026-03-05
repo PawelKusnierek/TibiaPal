de12bf78d15b79c564132142b5f94938ee178243
Kusnier <31161519+PawelKusnierek@users.noreply.github.com>
2026-03-05 11:52:53 +0100

    Disabling embed + removing some outdated code

--- _site/scripts/onload.js
+++ _site/scripts/onload.js
@@ -1,70 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //cookie_notice();
-  //find_rashid_city();
   enable_expandable_div_buttons();
   enable_default_tabs();
   enable_tablinks();
   //comment out below to disable kick embed - without the check it will never be visible/active
-  check_livestream();
+  //check_livestream();
 });
 
-function find_rashid_city() {
-  time = new Intl.DateTimeFormat("en-GB", {
-    dateStyle: "full",
-    timeStyle: "short",
-    timeZone: "Europe/Berlin",
-  }).format(new Date());
-  first_comma = time.indexOf(",");
-  day = time.substring(0, first_comma);
-  hour = parseInt(time.substring(time.length - 5, time.length - 3));
 
-  content_city = document.getElementById("rashid-city");
-
-  if (hour > 9) {
-    city = find_city_based_on_day(day);
-    content_city.innerHTML = "Rashid in " + city;
-  } else {
-    previous_day = find_previous_day(day);
-    city = find_city_based_on_day(previous_day);
-    content_city.innerHTML = "Rashid in " + city;
-  }
-}
-
-function find_city_based_on_day(day) {
-  if (day == "Monday") {
-    return "Svargrond";
-  } else if (day == "Tuesday") {
-    return "Liberty Bay";
-  } else if (day == "Wednesday") {
-    return "Port Hope";
-  } else if (day == "Thursday") {
-    return "Ankrahmun";
-  } else if (day == "Friday") {
-    return "Darashia";
-  } else if (day == "Saturday") {
-    return "Edron";
-  } else if (day == "Sunday") {
-    return "Carlin";
-  }
-}
-
-function find_previous_day(day) {
-  if (day == "Monday") {
-    return "Sunday";
-  } else if (day == "Tuesday") {
-    return "Monday";
-  } else if (day == "Wednesday") {
-    return "Tuesday";
-  } else if (day == "Thursday") {
-    return "Wednesday";
-  } else if (day == "Friday") {
-    return "Thursday";
-  } else if (day == "Saturday") {
-    return "Friday";
-  } else if (day == "Sunday") {
-    return "Saturday";
-  }
-}
 
 function enable_expandable_div_buttons() {
   var coll = document.getElementsByClassName("expandable-div");
@@ -80,14 +22,6 @@ function enable_expandable_div_buttons() {
         content.style.display = "block";
       }
     });
-  }
-}
-
-function cookie_notice() {
-  cookies_consented = localStorage.getItem("cookieconsent");
-  // if cookies not consented to, display cookie banner
-  if (!cookies_consented) {
-    document.querySelector(".cookieconsent").style.display = "initial";
   }
 }
 
