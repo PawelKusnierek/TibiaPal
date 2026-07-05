58b91e57c42dc35cba38eda8fc106748ee85286f
Kikaro <kik-tibia@users.noreply.github.com>
2026-07-05 17:18:50 +0100

    Fix floor/ceil for exp share

--- scripts/expshare.js
+++ scripts/expshare.js
@@ -2,8 +2,8 @@ function submit_form() {
     level = document.getElementById("level").value
     content = document.getElementById("expresults")
     content.innerHTML = "";
-    lower_range = Math.ceil(level/1.5)
-    upper_range = Math.floor(level*1.5)
+    lower_range = Math.max(1, Math.floor(level/1.5))
+    upper_range = Math.ceil(level*1.5)
     upper_range += level % 2 === 0 ? 1 : 0
     content.innerHTML = content.innerHTML + "A character with level <b>" + level +"</b> can share experience with levels <b>" + lower_range + "</b> to <b>" + upper_range +"</b><br>"
 }
